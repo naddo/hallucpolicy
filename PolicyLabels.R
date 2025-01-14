57dd0207e160fd977b8610706b3a8df77957a9ee
naddo <newton.addo@gmail.com>
2025-01-14 09:56:44 -0800

    Create PolicyLabels.R

---
+++ PolicyLabels.R
@@ -0,0 +1,62 @@
+outcome_list_label = list(
+  opioid_use = "Opioid Use Disorder",
+  pain_use = "Use Disorder - Pain Meds",
+  her_use = "Use Disorder - Heroin",
+  psilcy_ever = "Psilocybin Ever",
+  halluc_ever = "CUPS Hallucinogen Ever",
+  hallucflag.y = "NSDUH Hallucinogen Ever",
+  halluc_p = "'Classic' Hallucinogen Ever",
+  hallucly = "NSDUH Hallucinogen in the Last Year",
+  herflag = "Ever Heroin Use",
+  pnranyflag = "Ever Presciption Pain Reliever Use",
+  any_opi = "Ever (Opioid) Presciption Pain/Heroin"
+)
+
+demo_var_list = list(
+  year  = "Year",
+  period_indic = "Period",
+  catag6 = "Age Category",
+  race_cat = "Race/Ethnicity",
+  irsex = "Sex",
+  eduhighcat = "Education",
+  irmarit = "Marital Status",
+  rskyfqtes = "Like to test yourself by doing Risky Things",
+  irwrkstat = "Employment",
+  income = "Income",
+  difobther = "Heroin fairly or very easy to obtain",
+  difobtlsd = "LSD fairly or very easy to obtain"
+)
+
+substance_list_label <- list(
+  halluc_p = "Classic Hallucinogen Ever",
+  lsd_ever = "Ever Use : LSD",
+  psilcy_ever = "Ever Use : Psilocybin",
+  ecstasy_ever = "Ever Use : Ecstasy",
+  ket_ever = "Ever Use : Ketamine",
+  pcp_ever = "Ever Use : PCP",
+  mesc_ever = "Ever Use : Mescaline",
+  dmt_ever = "Ever Use : DMT",
+  salv_ever = "Ever Use : Salvia",
+  mj_ever = "Ever Use : Marijuana",
+  coc_ever = "Ever Use : Cocaine",
+  stm_ever = "Ever Use : Stimulants",
+  trq_ever = "Ever Use : Tranquilizers",
+  inh_ever = "Ever Use : Inhalants",
+  sed_ever = "Ever Use : Sedatives",
+  halluc_ever = "CUPS Hallucinogen Ever",
+  meth_ever = "Ever Use : Methamphetamines",
+  peyote_ever = "Ever Use : Peyote"
+)
+
+criteria_labels = list(
+  opioid_dsm_1 = "Used Opioid in LARGER AMOUNTS/ Longer time than meant to",
+  opioid_dsm_2 = "Unable to cut/stop /CUTDOWN",
+  opioid_dsm_3 = "Great deal/ time GETTING/USING or under the influence",
+  opioid_dsm_4 = "Increase URGE/need to use",
+  opioid_dsm_5 = "Cause serious PROBLEMS at home/work/sch",
+  opioid_dsm_6 = "Continued used despite arguments/PROBLEMS w/family/friends",
+  opioid_dsm_7 = "Less ACTIVITIES due to use",
+  opioid_dsm_8 = "Increase chance of getting HURT",
+  opioid_dsm_9 = "Continued used despite HEALTH PROBLEMS",
+  opioid_dsm_10 = "Increase amount used to desired EFFECT",
+  opioid_dsm_12 = "3 or more WD SYMPTOMS")
